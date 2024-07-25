460df74b191d1b0062bd8cc82f3eba763f1c2b5b
im-e <immie@riseup.net>
2024-07-25 09:36:06 +0100

    added files

---
+++ src/main/java/com/imi/gracerasis/GraceRasisApplication.java
@@ -0,0 +1,13 @@
+package com.imi.gracerasis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraceRasisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraceRasisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/imi/gracerasis/GraceRasisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imi.gracerasis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraceRasisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
