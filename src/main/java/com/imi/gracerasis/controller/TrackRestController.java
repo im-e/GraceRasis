6dfebf0656e619ecd40a3e55e103e14dd7ad5d4f
im-e <immie@riseup.net>
2024-07-25 10:14:51 +0100

    added frontend

---
+++ src/main/java/com/imi/gracerasis/controller/TrackRestController.java
@@ -0,0 +1,35 @@
+package com.imi.gracerasis.controller;
+
+import com.imi.gracerasis.service.NearNoahService;
+import com.imi.gracerasis.service.TachiService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TrackRestController {
+
+    private final NearNoahService nearNoahService;
+    private final TachiService tachiService;
+
+    public TrackRestController(NearNoahService nearNoahService, TachiService tachiService) {
+        this.nearNoahService = nearNoahService;
+        this.tachiService = tachiService;
+    }
+
+    @GetMapping("/track")
+    public String getTrackByLevel(@RequestParam int level) {
+        return nearNoahService.getTrackData(level);
+    }
+
+    @GetMapping("/track")
+    public String getTrackByArtist(@RequestParam String artist) {
+        return nearNoahService.getTrackData(artist);
+    }
+
+    @GetMapping("/tracks")
+    public String getAllTracks() {
+        return nearNoahService.getTrackData();
+    }
+
+}
