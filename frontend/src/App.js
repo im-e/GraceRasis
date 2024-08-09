3c74dac1747e1e263ab518532751e91af1d14384
im-e <immie@riseup.net>
2024-08-09 11:08:11 +0100

    restarted react project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import HomePage from "./components/HomePage";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-      <Router>
-        <div>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<HomePage/>} />
-          </Routes>
-          <Footer />
-        </div>
-      </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
